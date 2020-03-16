2c9fc055f7d98026c4c0994d156f9b7d8ae41076
Edwin Kerforne <edkerforne@users.noreply.github.com>
2020-03-16 19:58:52 +0100

    Fix error rendering

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
 /*
  * Handle errors
  */
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   // Only provide errors in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
