9d4b34f1178ca003f955cc18e0bafa47acb90022
Edwin Kerforne <edkerforne@users.noreply.github.com>
2020-03-22 19:12:33 +0100

    Implement create author form

--- controllers/author.js
+++ controllers/author.js
@@ -1,8 +1,11 @@
 const Author = require('../models/author');
 const Book = require('../models/book');
 const async = require('async');
+const { body, validationResult } = require('express-validator');
 
-// Display list of all authors
+/*
+ * Display list of all authors
+ */
 exports.list = (req, res, next) => {
   Author.find()
     .populate('author')
@@ -13,7 +16,9 @@ exports.list = (req, res, next) => {
     });
 };
 
-// Display page of a specific author
+/*
+ * Display page of a specific author
+ */
 exports.page = (req, res, next) => {
   async.parallel({
     author: callback => {
@@ -38,32 +43,101 @@ exports.page = (req, res, next) => {
   });
 };
 
-// Display author create form on GET
+/*
+ * Display author create form on GET
+ */
 exports.createGet = (req, res) => {
-  res.send('TODO: Author create GET');
+  res.render('authorForm', { title: 'Add an author' });
 };
 
-// Handle author create on POST
-exports.createPost = (req, res) => {
-  res.send('TODO: Author create POST');
-};
+/*
+ * Handle author create on POST
+ */
+exports.createPost = [
 
-// Display author delete form on GET
+  // Validate fields
+  body('firstName')
+    .isLength({ min: 1 }).trim().withMessage('Must not be empty')
+    .isAlphanumeric().withMessage('Must only contain alphanumeric characters'),
+
+  body('lastName')
+    .isLength({ min: 1 }).trim().withMessage('Must not be empty')
+    .isAlphanumeric().withMessage('Must only contain alphanumeric characters'),
+
+  body('dateOfBirth', 'Invalid date')
+    .optional({ checkFalsy: true }).isISO8601(),
+
+  body('dateOfDeath', 'Invalid date')
+    .optional({ checkFalsy: true }).isISO8601(),
+
+  // Sanitize input
+  body('firstName').escape(),
+  body('lastName').escape(),
+  body('dateOfBirth').toDate(),
+  body('dateOfDeath').toDate(),
+
+  // Process request after validation and sanitization
+  (req, res, next) => {
+
+    // Extract validation errors from the request
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+
+      // There are errors, so render the form again with
+      // sanitized values & error messages
+      res.render('authorForm', { 
+        title: 'Add an author',
+        author: req.body,
+        errors: errors.array()
+      });
+      return;
+    } else {
+
+      // Form data is valid, create an author with
+      // escaped and trimmed data
+      const author = new Author({
+        name: {
+          first: req.body.firstName,
+          last: req.body.lastName
+        },
+        dateOfBirth: req.body.dateOfBirth,
+        dateOfDeath: req.body.dateOfDeath
+      });
+
+      author.save((err) => {
+        if (err) return next(err);
+
+        // Author saved, redirect to its page
+        res.redirect(author.url);
+      });
+    }
+  }
+];
+/*
+ *  Display author delete form on GET
+ */
 exports.deleteGet = (req, res) => {
   res.send('TODO: Author delete GET');
 };
 
-// Handle author delete on POST
+/*
+ * Handle author delete on POST
+ */
 exports.deletePost = (req, res) => {
   res.send('TODO: Author delete POST');
 };
 
-// Display author update form on GET
+/*
+ * Display author update form on GET
+ */
 exports.updateGet = (req, res) => {
   res.send('TODO: Author update GET');
 };
 
-// Handle author update on POST
+/*
+ * Handle author update on POST
+ */
 exports.updatePost = (req, res) => {
   res.send('TODO: Author update POST');
 };
